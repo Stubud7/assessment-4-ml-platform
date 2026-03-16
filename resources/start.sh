6205945293eab5123c7a2233fd302f7e40f09afc
okinoleiba <okino.leiba@yahoo.com>
2026-03-16 03:40:28 +0000

    first commit

--- resources/start.sh
+++ resources/start.sh
@@ -37,6 +37,9 @@ DASHBOARD_PORT=$(find_available_port 3000)
 [ "$BACKEND_PORT" != "8000" ] && echo "Port 8000 in use, using $BACKEND_PORT"
 [ "$DASHBOARD_PORT" != "3000" ] && echo "Port 3000 in use, using $DASHBOARD_PORT"
 
+echo "Get EKS cluster name"
+EKS_CLUSTER="$(terraform output -raw eks_cluster_name)"
+
 if [ ! -f .env ]; then
     cat > .env << EOF
 AWS_REGION=us-east-1
@@ -46,6 +49,7 @@ DASHBOARD_PORT=$DASHBOARD_PORT
 TF_VAR_project_name=ml-platform
 TF_VAR_environment=dev
 TF_VAR_aws_region=us-east-1
+EKS_CLUSTER=$EKS_CLUSTER
 EOF
     echo "Created .env"
 else
@@ -56,6 +60,7 @@ if [ ! -f .env.secrets ]; then
     cat > .env.secrets << EOF
 # AWS_ACCESS_KEY_ID=
 # AWS_SECRET_ACCESS_KEY=
+# GHCR_TOKEN=
 EOF
     echo "Created .env.secrets"
 else
