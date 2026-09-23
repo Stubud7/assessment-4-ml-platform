6c71a65b63ce413936b402e0556792e4c6ff4c6a
Stuart_Anderson <stubud777@gmail.com>
2026-09-22 17:39:04 -0700

    add pem key updates

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -11,6 +11,15 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
+data "aws_key_pair" "existing" { 
+  key_name = "stuart2-key" 
+  }
+  
+
+
+
+
+
 # Backend EC2 Server Instance
 resource "aws_instance" "backend" {
   ami                    = data.aws_ami.amazon_linux.id
@@ -18,23 +27,25 @@ resource "aws_instance" "backend" {
   subnet_id              = data.aws_subnet.existing_public.id
   vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+  key_name = data.aws_key_pair.existing.key_name
 
   user_data = <<-EOF
     #!/bin/bash
+    exec > /var/log/user_data.log 2>&1
+    set -x
+
     sudo dnf update -y
-    sudo dnf install -y git docker
+    sudo dnf install -y git docker docker-compose-plugin
     sudo systemctl start docker
     sudo systemctl enable docker
     sudo usermod -aG docker ec2-user
 
-    # Install Docker Compose plugin
-    sudo dnf install -y docker-compose-plugin
-
-    # Clone repo and launch Gateway + Dashboard containers
     cd /home/ec2-user
     git clone https://github.com/stubud7/assessment-4-ml-platform.git app
     cd app
-    docker compose up -d --build
+    
+    # Use sudo here so cloud-init has permission to interact with the Docker socket
+    sudo docker compose up -d --build
 EOF
 
   tags = {
